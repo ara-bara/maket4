daa5dbe5a662078e7c3f6c7b1e9a261b60f5d1ab
ara-bara <archomoris2510@gmail.com>
2023-04-18 11:38:22 +0300

    burger menu for header

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-/*slider*/
+/*burger menu for header*/
+let header__button = document.querySelector('.header__button');
+let header__list = document.querySelector('.header__list');
+header__button.addEventListener('click', function(){
+    header__button.classList.toggle('active');
+    header__list.classList.toggle('active');
+})
 
-/*$(document).ready(function(){
-    $('.course-outline__slider').slick({
-        arrows:false,
-        dots:true,
-        autoplay: true,
-        autoplaySpeed: 10000,
-        fade: true
+/*course-outline*/
+$(document).ready(function(){
+    $('.course-outline__list').slick({
+        arrows:         false,
+        autoplay:       true,
+        autoplaySpeed:  4000,
+        infinite:       false,
+        pauseOnFocus:   false,
+        pauseOnHover:   false,
+        slidesToShow:   3,
+        slidesToScroll: 1,
+        responsive:[
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 
+                }
+            }
+        ]
     });
-});*/
+})
+
+/* testimonials*/
 $(document).ready(function(){
     $('.slider').slick({
         arrows:false,
@@ -19,6 +45,7 @@ $(document).ready(function(){
     });
 });
 
+/* mentors */
 $(document).ready(function(){
     $('.mentors__list').slick({
         arrows:false,
